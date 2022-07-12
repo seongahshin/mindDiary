5bde7ccc2b1120e08dfe283044dccf75f214623d
seongahshin <90595710+seongahshin@users.noreply.github.com>
2022-07-12 18:55:13 +0900

    Alert 기능 추가

--- mindDiary/ViewController.swift
+++ mindDiary/ViewController.swift
@@ -41,8 +41,6 @@ class ViewController: UIViewController {
     var resultNumber: [Int] = [0,0,0,0,0,0,0,0,0]
     var keyWord: [String] = ["사랑해","좋아해","당황해","속상해","우울해","심심해","행복해","화가나","슬퍼"]
     
-    var answer: String = ""
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -63,9 +61,27 @@ class ViewController: UIViewController {
         appTitle.textColor = .black
     }
     
+    func showAlertController() {
+        // 1. 흰 바탕 : UIAlertController
+        let alert = UIAlertController(title: "알림", message: "오늘의 감정 일기를 저장하시겠습니까?", preferredStyle: .alert)
+        
+        // 2. 버튼
+        let ok = UIAlertAction(title: "네", style: .destructive, handler: nil)
+        let cancel = UIAlertAction(title: "아니오", style: .cancel, handler: nil)
+
+        // 3. 흰 바탕에 버튼 추가 (이 순서대로 추가됨)
+        alert.addAction(cancel)
+        alert.addAction(ok)
+    
+        
+        // 4. present
+        // completion 은 내부적으로 정보 얻을 때
+        present(alert, animated: true, completion: nil)
+    }
     
     func resultLable(_ labelName: UILabel, buttonName: UIButton) {
         labelName.text = #"\#(keyWord[buttonName.tag]) \#(resultNumber[buttonName.tag])"#
+        showAlertController()
     }
     
     @IBAction func buttonClicked(_ sender: UIButton) {
@@ -112,16 +128,6 @@ class ViewController: UIViewController {
         resultNumber[sender.tag] += 1
         resultLable(Result9, buttonName: button9)
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
 }
 
